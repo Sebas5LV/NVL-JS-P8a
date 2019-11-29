2fa0c19e50b6340e49b32ee2f5f31fc51f1f7033
Sebas5LV <56948689+Sebas5LV@users.noreply.github.com>
2019-11-29 18:01:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var a ="Amigo"
+alert(a.toUpperCase(1,4))
+
+//Ahora nos debería mostrar las restantes cuatro letras en mayúsculas
+
+alert(a.toLowerCase())
+
+//Ahora nos debería mostrar todo el texto en minúscula
+
+alert(a.toLowerCase(0))
+
+//Debería convertir únicamente la primera letra en minúscula
+
+alert(a.replace("A","a"))
+
+//Debería reemplazar la A por a
